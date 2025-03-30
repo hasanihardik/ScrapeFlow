319cb358c76bd923d4d02036d27c5013c77fbd33
hasanihardik <hasanihardik@gmail.com>
2025-03-30 10:15:06 +0530

    puppeteer fixes

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,13 @@
+const {join} = require('path');
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  // Ensure Chrome is downloaded
+  chrome: {
+    skipDownload: false
+  }
+};
